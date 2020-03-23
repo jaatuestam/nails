44a0ee198ea532d270891be1a5f9a0be7c826da1
jorge <jatuest@bancodebogota.com.co>
2020-03-22 20:01:50 -0500

    actualizaciones para heroku

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/nails'));
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/dist/nails/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
